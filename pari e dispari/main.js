90f2a22641ad3bedf85693de2f405cf5cb3ca938
defendimattia <defendi.mattia92@gmail.com>
2025-02-28 17:01:43 +0100

    fix: easier code, but not the best solution

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -10,17 +10,17 @@ if (userNum >= 1 && userNum <= 5) {
     // - dichiariamo una variabile dove verrà richiamata la funzione
     const sumNumOddOrEven = isNumOddOrEven(sumNum);
     // - printiamo il risultato
-    if ((sumNumOddOrEven && inputOddOrEven === "pari") || (sumNumOddOrEven === false && inputOddOrEven === "dispari")) console.log("Hai indovinato!");
+    if ((sumNumOddOrEven && inputOddOrEven === "pari") || (!sumNumOddOrEven && inputOddOrEven === "dispari")) console.log("Hai indovinato!");
     else console.log("Non hai indovinato!");
 };
 
 
 // - creiamo una funzione dove generiamo un numero random da 1 a 5
 function random1to5() {
-    return Math.floor(Math.random() * 5) + 1
+    return Math.floor(Math.random() * 5) + 1;
 };
 
 // - creiamo una funzione dove restituisce un valore booleano a seconda se il risultato della variabile di prima è pari o dispari
 function isNumOddOrEven(num) {
-    return num % 2 === 0
+    return num % 2 === 0;
 };
